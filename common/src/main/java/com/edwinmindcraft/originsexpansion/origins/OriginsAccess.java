beaf2c5969275bb8d385aa692cee16edad38533f
EdwinMindcraft <edwin.mindcraft@gmail.com>
2021-08-15 13:26:55 +0200

    Updated to origins 0.7.3 and architectury 3.2

--- common/src/main/java/com/edwinmindcraft/originsexpansion/origins/OriginsAccess.java
+++ common/src/main/java/com/edwinmindcraft/originsexpansion/origins/OriginsAccess.java
@@ -1,11 +1,11 @@
 package com.edwinmindcraft.originsexpansion.origins;
 
+import dev.architectury.injectables.annotations.ExpectPlatform;
 import io.github.apace100.origins.component.OriginComponent;
 import io.github.apace100.origins.power.factory.action.ActionFactory;
 import io.github.apace100.origins.power.factory.action.ActionType;
 import io.github.apace100.origins.power.factory.condition.ConditionFactory;
 import io.github.apace100.origins.power.factory.condition.ConditionType;
-import me.shedaniel.architectury.annotations.ExpectPlatform;
 import me.shedaniel.architectury.registry.Registry;
 import net.minecraft.world.entity.Entity;
 
